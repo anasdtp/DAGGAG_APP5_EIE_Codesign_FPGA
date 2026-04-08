0b5dcc89676b60b17712e6830085e765e24a8446
Anas DAGGAG Polytech <anas.daggag@gmail.com>
2026-04-08 15:55:21 +0200

    le projet compile sur altera

---
+++ altera/obj/default/C/Users/mdaggag/Documents/Quartus/DAGGAG_APP5_EIE_Codesign_FPGA/software/validation_calculateur.d
@@ -0,0 +1,3 @@
+ \
+ obj/default//C/Users/mdaggag/Documents/Quartus/DAGGAG_APP5_EIE_Codesign_FPGA/software/validation_calculateur.o:  \
+ C:/Users/mdaggag/Documents/Quartus/DAGGAG_APP5_EIE_Codesign_FPGA/software/validation_calculateur.c
